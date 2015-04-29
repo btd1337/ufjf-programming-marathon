13f8354494aaf7177bfa8ba0e69cbab77b061927
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2015-04-29 20:03:34 -0300

    3n+1 updated

--- Programming Challenges/1.6.1 The 3n+1 Problem/main.cpp
+++ Programming Challenges/1.6.1 The 3n+1 Problem/main.cpp
@@ -1,25 +1,35 @@
-#include <iostream>
-using namespace std;
+    #include <iostream>
+    using namespace std;
 
-int main()
-{
-    int a,b,i,j,maximo=1,ciclo=0;
-
-    cin >> a >> b ;
-
-    for(i=a;i<=b;i++)
+    int main()
     {
-        for(j=i;j!=1;ciclo++)
-        {
-            if(j%2==0)
-                j/=2;
-            else if(j%2==1)
-                j=(j*3)+1;
+        int a,b,from,to,maximo=1,ciclo=0;
+
+        while(cin >> a >> b){
+            if(a > 0 && a < 1000000 && b > 0 && b < 1000000){
+                maximo = 1;
+                if(a > b){
+                    from = b;
+                    to = a;
+                }else{
+                    from = a;
+                    to = b;
+                }
+                for(int i=from;i<=to;i++)
+                {
+                    for(int j=i;j!=1;ciclo++)
+                    {
+                        if(j%2==0)
+                            j/=2;
+                        else if(j%2==1)
+                            j=(j*3)+1;
+                    }
+                    if(ciclo>maximo)
+                        maximo=ciclo;
+                    ciclo=0;
+                }
+                cout << a << " " << b << " " << maximo+1 << endl;
+            } 
         }
-        if(ciclo>maximo)
-            maximo=ciclo;
-        ciclo=0;
+        return 0;
     }
-    cout << a << " " << b << " " << maximo+1 << endl;
-    return 0;
-}
