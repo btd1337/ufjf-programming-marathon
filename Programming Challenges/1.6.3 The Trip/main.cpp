078e20c04feb36f344f56dd6a61a8f522a3f1576
Mateus Nery Franco <mateusnery.ice@gmail.com>
2015-04-30 16:04:57 -0300

    30/04/15 uptade The trip + Interpreter

--- Programming Challenges/1.6.3 The Trip/main.cpp
+++ Programming Challenges/1.6.3 The Trip/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    int numPessoas;
+    int numPessoas, k=0;
+    float restante[1000], media=0, soma=0;
+    float gastos[1000];
 
-    cin >> numPessoas;
-
-    while (numPessoas != 0){
-        cin >> numPessoas;
+    while (cin >> numPessoas)
+    {
+        cout << "a" << endl;
+        if (numPessoas == 0)
+            break;
+        for(int i=0; i<numPessoas; i++)
+        {
+            cin >> gastos[i];
+            soma += gastos[i];
+        }
+        media=soma/numPessoas;
+        for(int i=0; i<numPessoas; i++)
+        {
+            if (gastos[i] > media)
+                restante[k]+=(gastos[i]-media);
+        }
+        k++;
     }
-
+    setprecision >> fixed(2);
+    for(int i=0; i<k; i++)
+    {
+        cout << "$" << restante[i] << endl;
+    }
     return 0;
 }
