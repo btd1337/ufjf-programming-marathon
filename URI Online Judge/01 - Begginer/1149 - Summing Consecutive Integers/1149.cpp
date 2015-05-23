504956cbbb3ae68850b9921017f0b1da3a4ef944
btd1334 <helder.bertoldo@ice.ufjf.br>
2015-05-23 12:58:30 -0300

    "Atualização de Pastas + Envio de Questões do URI"

---
+++ URI Online Judge/01 - Begginer/1149 - Summing Consecutive Integers/1149.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, N,sum=0;
+    cin >> A >> N;
+    while(N<=0)
+        cin >> N;
+    for(int i=1; i<= N; i++)
+    {
+        sum+=A;
+        A++;
+    }
+    cout << sum << endl;
+    return 0;
+}
