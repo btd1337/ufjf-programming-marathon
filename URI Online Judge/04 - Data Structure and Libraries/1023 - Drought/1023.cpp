64873bab47642dedf52c99e67f0aa5f2eef33e14
btd1334 <helder.bertoldo@ice.ufjf.br>
2015-05-27 05:01:38 -0300

    "URI - Solucionados: 1023,1158"

--- URI Online Judge/04 - Data Structure and Libraries/1023 - Drought/1023.cpp
+++ URI Online Judge/04 - Data Structure and Libraries/1023 - Drought/1023.cpp
@@ -24,62 +24,63 @@ int main()
 {
     cout << fixed << setprecision(2);
     int cid=1,auxQtde,i,j,N=1;
-    vector<tinfConsumo> x(1000);
+    vector<tinfConsumo> x(1000000);
+    int itFim;
+    bool flag = false;
+
     while(N!=0)
     {
-        int qtdeMC=0,cTotalC=0;
+        long qtdeMC=0,cTotalC=0;
         cin >> N;
+        if(N==0)
+            break;
         for(int i=0; i < N; i++)
         {
             cin >> x[i].qtdeMoradores >> x[i].consumoTotal;
             x[i].consumoMedio = (x[i].consumoTotal)/(x[i].qtdeMoradores);
             qtdeMC+=x[i].qtdeMoradores;
             cTotalC+=x[i].consumoTotal;
+            itFim = i;
         }
 
         sort(x.begin(),x.begin()+N,compare);
 
+        if(flag)
+            cout << endl;
         cout << "Cidade# " << cid << ":" << endl;
         for(int i=0; i<N;)
         {
             j=1;
             auxQtde=0;
-            if((&x[i+j]!=&x[N]) && (x[i].consumoMedio == x[i+j].consumoMedio))
+            if((&x[i]!=&x[itFim]) && (x[i].consumoMedio == x[i+j].consumoMedio))
             {
-                while((&x[i+j]!=&x[N]) && (x[i].consumoMedio == x[i+j].consumoMedio))
-                {
-                    auxQtde += x[j].qtdeMoradores;
-                    j++;
-                }
-                cout << x[i].qtdeMoradores+auxQtde << "-" << x[i].consumoMedio;
-                i+=j;
-
+                    while(i+j<=itFim && (x[i].consumoMedio == x[i+j].consumoMedio))
+                    {
+                        auxQtde += x[i+j].qtdeMoradores;
+                        j++;
+                    }
+                    cout << x[i].qtdeMoradores+auxQtde << "-" << x[i].consumoMedio;
+                    i+=j;
             }
             else
             {
                 cout << x[i].qtdeMoradores << "-" << x[i].consumoMedio;
                 i++;
             }
-            if(&x[i]==&x[N])
-                cout << endl;
-            else
+            if(i<=itFim)
                 cout << " ";
         }
 
         //Trunc
-        double b = (double) cTotalC / qtdeMC;
-        int c = b*100;
+        double b = (double)cTotalC/qtdeMC;
+        long c = b*100;
         double trunc = (double)c/100;
 
-        cout << "Consumo medio: " << trunc << " m3.";
+        cout << endl << "Consumo medio: " << trunc << " m3." << endl;
         //Fim
 
         cid++;
-        if(cid!=N)
-            cout << endl;
-
-
-
+        flag = true;
     }
 
     return 0;
