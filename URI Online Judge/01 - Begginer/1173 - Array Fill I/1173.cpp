577c73e9a0285ef98b677558a6574acba84e80d8
btd1334 <helder.bertoldo@ice.ufjf.br>
2015-05-29 01:40:57 -0300

    "URI - Solucionados: 1025,1164,1165,1172,1173"

---
+++ URI Online Judge/01 - Begginer/1173 - Array Fill I/1173.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,N[10];
+    cin >> x;
+    N[0]=x;
+    cout << "N[0] = " << N[0] << endl;
+    for(int i=1;i<10;i++){
+        N[i]=N[i-1]*2;
+        cout << "N[" << i << "] = " << N[i] << endl;
+    }
+    return 0;
+}
